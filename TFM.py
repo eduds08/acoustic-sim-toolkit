59150c66e1db09977566d33e00179c1fec35f3e7
eduds08 <edudscrc@outlook.com>
2024-08-08 19:35:27 -0300

    started TFM class

---
+++ TFM.py
@@ -0,0 +1,169 @@
+import numpy as np
+import os
+from WebGPUConfig import WebGPUConfig
+from plt_utils import save_imshow
+from os_utils import clear_folder
+
+
+class TFM(WebGPUConfig):
+    def __init__(self, **simulation_config):
+        super().__init__(**simulation_config)
+
+        self.shader_file = './tfm.wgsl'
+
+        self.folder = './TFM'
+
+        os.makedirs(self.folder, exist_ok=True)
+
+        clear_folder(self.folder)
+
+        self.ac_sim_folder = './AcousticSimulation'
+        self.ac_receptors_folder = f'{self.ac_sim_folder}/receptors_setup'
+        self.ac_recorded_pressure_folder = f'{self.ac_sim_folder}/recorded_pressure'
+        self.ac_reflectors_folder = f'{self.ac_sim_folder}/reflectors_setup'
+
+        # Reflectors setup
+        if len(os.listdir(f'{self.ac_reflectors_folder}')) != 0:
+            self.number_of_reflectors = np.load(f'{self.ac_reflectors_folder}/number_of_reflectors.npy')
+            self.reflector_z = np.load(f'{self.ac_reflectors_folder}/reflector_z.npy')
+            self.reflector_x = np.load(f'{self.ac_reflectors_folder}/reflector_x.npy')
+
+        # Receptors setup
+        self.number_of_receptors = np.load(f'{self.ac_receptors_folder}/number_of_receptors.npy')
+        self.receptor_z = np.load(f'{self.ac_receptors_folder}/receptor_z.npy')
+        self.receptor_x = np.load(f'{self.ac_receptors_folder}/receptor_x.npy')
+
+        self.recorded_pressure = []
+
+        # Recorded pressure on receptors
+        for i in range(self.number_of_receptors):
+            recorded_pressure = np.array(np.load(
+                f'{self.ac_recorded_pressure_folder}/receptor_{i}.npy'
+            ), dtype=np.float32)
+
+            self.recorded_pressure.append(recorded_pressure)
+
+        self.info_int = np.array(
+            [
+                self.grid_size_z,
+                self.grid_size_x,
+                self.number_of_receptors,
+                0,
+            ],
+            dtype=np.int32
+        )
+
+        self.info_float = np.array(
+            [
+                self.dz,
+                self.dx,
+                self.dt,
+                self.c,
+            ],
+            dtype=np.float32
+        )
+
+    def run(self, plt_kwargs=None):
+        if plt_kwargs is None:
+            plt_kwargs = {}
+
+        shader_file = open(self.shader_file)
+        shader_string = shader_file.read().replace('wsz', f'{self.wsz}').replace('wsx', f'{self.wsx}')
+
+        aux_string = ''
+        for i in range(self.number_of_receptors):
+            aux_string += f'''@group(0) @binding({i + 8})
+var<storage,read> recorded_pressure_{i}: array<f32>;\n\n'''
+
+        shader_string = shader_string.replace('//RECORDED_PRESSURE_BINDINGS', aux_string)
+
+        aux_string = ''
+        for i in range(self.number_of_receptors):
+            aux_string += f'''if (receptor_idx == {i})
+            {{
+                p_future[zx(z, x)] += recorded_pressure_{i}[infoI32.i];
+            }}\n'''
+
+        shader_string = shader_string.replace('//RECORDED_PRESSURE_SIM', aux_string)
+
+        shader_file.close()
+
+        self.shader_module = self.device.create_shader_module(code=shader_string)
+
+        wgsl_data = {
+            'infoI32': self.info_int,
+            'infoF32': self.info_float,
+            'receptor_z': self.receptor_z,
+            'receptor_x': self.receptor_x,
+            'p_future': self.p_future,
+            'p_present': self.p_present,
+            'p_past': self.p_past,
+            'lap': self.lap,
+        }
+
+        for i in range(self.number_of_receptors):
+            wgsl_data[f'recorded_pressure_{i}'] = self.recorded_pressure[i]
+
+        shader_lines = list(shader_string.split('\n'))
+        buffers = self.create_buffers(wgsl_data, shader_lines)
+
+        compute_lap = self.create_compute_pipeline("laplacian_5_operator")
+        compute_sim = self.create_compute_pipeline("sim")
+        compute_incr = self.create_compute_pipeline("incr_time")
+
+        # if len(os.listdir(f'{self.ac_reflectors_folder}')) != 0:
+        #     scatter_kwargs = {
+        #         'number_of_reflectors': self.number_of_reflectors,
+        #         'reflector_z': self.reflector_z,
+        #         'reflector_x': self.reflector_x,
+        #         'number_of_receptors': self.number_of_receptors,
+        #         'receptor_z': self.receptor_z,
+        #         'receptor_x': self.receptor_x,
+        #     }
+        # else:
+        #     scatter_kwargs = {
+        #         'number_of_receptors': self.number_of_receptors,
+        #         'receptor_z': self.receptor_z,
+        #         'receptor_x': self.receptor_x,
+        #     }
+
+        for i in range(self.total_time):
+            command_encoder = self.device.create_command_encoder()
+            compute_pass = command_encoder.begin_compute_pass()
+
+            for index, bind_group in enumerate(self.bind_groups):
+                compute_pass.set_bind_group(index, bind_group, [], 0, 999999)
+
+            compute_pass.set_pipeline(compute_lap)
+            compute_pass.dispatch_workgroups(self.grid_size_z // self.wsz,
+                                             self.grid_size_x // self.wsx)
+
+            compute_pass.set_pipeline(compute_sim)
+            compute_pass.dispatch_workgroups(self.grid_size_z // self.wsz,
+                                             self.grid_size_x // self.wsx)
+
+            compute_pass.set_pipeline(compute_incr)
+            compute_pass.dispatch_workgroups(1)
+
+            compute_pass.end()
+            self.device.queue.submit([command_encoder.finish()])
+
+            """ READ BUFFERS """
+            self.p_future = (np.asarray(self.device.queue.read_buffer(buffers['b4']).cast("f"))
+                             .reshape(self.grid_size_shape))
+
+            # if create_animation:
+            #     save_imshow(
+            #         data=self.p_future,
+            #         title=f'TFM',
+            #         path=f'{self.animation_folder}/plot_{i}.png',
+            #         scatter_kwargs=scatter_kwargs,
+            #         plt_kwargs=plt_kwargs,
+            #         plt_grid=True,
+            #         plt_colorbar=True,
+            #     )
+
+            if i % 100 == 0:
+                print(f'TFM - i={i}')
+
+        print('TFM finished.')
